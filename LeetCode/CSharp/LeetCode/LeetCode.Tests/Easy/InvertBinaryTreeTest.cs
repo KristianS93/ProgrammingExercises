f4ebad22f7c742fc603f93d1e950d7bc38f6a6bd
Desktop <KristianS93@hotmail.com>
2023-08-26 00:06:50 +0200

    added invert binary tree

---
+++ LeetCode/CSharp/LeetCode/LeetCode.Tests/Easy/InvertBinaryTreeTest.cs
@@ -0,0 +1,90 @@
+﻿using LeetCode.Easy;
+
+namespace LeetCode.Tests.Easy;
+
+public class InvertBinaryTreeTest
+{
+    [Fact]
+    public void Sample1()
+    {
+        var worker = new InvertBinaryTree();
+
+        var root = new TreeNode
+        {
+            val = 4,
+            left = new TreeNode
+            {
+                val = 2,
+                left = new TreeNode
+                {
+                    val = 1,
+                    left = null,
+                    right = null,
+                },
+                right = new TreeNode
+                {
+                    val = 3,
+                    left = null,
+                    right = null,
+                },
+            },
+            right = new TreeNode
+            {
+                val = 7,
+                left = new TreeNode
+                {
+                    val = 6,
+                    left = null,
+                    right = null
+                },
+                right = new TreeNode
+                {
+                    val = 9,
+                    left = null,
+                    right = null
+                }
+            }
+        };
+
+        var expected = new TreeNode
+        {
+            val = 4,
+            left = new TreeNode
+            {
+                val = 7,
+                left = new TreeNode
+                {
+                    val = 9,
+                    left = null,
+                    right = null,
+                },
+                right = new TreeNode
+                {
+                    val = 6,
+                    left = null,
+                    right = null,
+                },
+            },
+            right = new TreeNode
+            {
+                val = 2,
+                left = new TreeNode
+                {
+                    val = 3,
+                    left = null,
+                    right = null
+                },
+                right = new TreeNode
+                {
+                    val = 1,
+                    left = null,
+                    right = null
+                }
+            }
+
+        };
+
+        var result = worker.InvertTree(root);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ LeetCode/CSharp/LeetCode/LeetCode/Easy/InvertBinaryTree.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Easy;
+
+public class InvertBinaryTree
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        // if (root == null)
+        // {
+        //     return null;
+        // }
+        //
+        // if (root.left != null && root.right != null)
+        // {
+        //     InvertTree(root.left);
+        //     InvertTree(root.right);
+        //     (root.left, root.right) = (root.right, root.left);
+        // }
+        //
+        // if (root.left != null && root.right == null)
+        // {
+        //     InvertTree(root.left);
+        //     (root.left, root.right) = (null, root.left);
+        // }
+        // if (root.left == null && root.right != null)
+        // {
+        //     InvertTree(root.right);
+        //     (root.left, root.right) = (root.right, null);
+        // }
+        //
+        // return root;
+        (root.left, root.right) = (InvertTree(root.right), InvertTree(root.left));
+        return root;
+    }
+}
+
+public class TreeNode {
+     public int val;
+     public TreeNode? left;
+     public TreeNode? right;
+     public TreeNode(int val=0, TreeNode? left=null, TreeNode? right=null) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+ }
